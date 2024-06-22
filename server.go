57a2c9cc44234d82b0a8b95857479174aa992be2
yanghy <snxamdf@126.com>
2024-06-22 19:15:13 +0800

    add assetserve

--- server.go
+++ server.go
@@ -1,29 +1,63 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License Version 2.0, January 2004
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+//----------------------------------------
+
+// 简易的内置静态资源http服务
+//	由于使用go http server该服务可能会引起一些安全软件报毒
+//	报毒解决方案
+//		1. 配置 ssl 证书, 未测试
+//		2. 编译时 使用 -ldflags "-s -w" 去除调试信息和符号
+//		3. 使用 upx 工具压缩执行文件
+//	最好是 2 和 3 配合使用
+
 package assetserve
 
 import (
+	"context"
 	"crypto/tls"
-	"embed"
 	"errors"
 	"fmt"
+	"github.com/energye/lcl/emfs"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
 var contentType = map[string]string{}
 
+// AssetsServerHeaderKeyName
+//
+//	用于保证链接的安全的请求头(header)key名称
+var AssetsServerHeaderKeyName = "ASSETS_SERVER_KEY"
+
+// AssetsServerHeaderKeyValue
+//
+//	用于保证链接的安全的key值
+//	这里简单的在请求所有资源时增加请求头的判断
+//	不为空时生效
+var AssetsServerHeaderKeyValue string
+
 type assetsHttpServer struct {
-	LocalAssets  string    //本地静态资源目录 示例: /app/assets/   http://127.0.0.1:8888/demo/demo.html -> /app/assets/demo/demo.html
-	AssetsFSName string    //静态资源内置FS目录名 示例: assets
-	Assets       *embed.FS //静态资源内置FS目录对象
-	IP           string    //
-	PORT         int       //
-	SSL          *SSL      //设置后启动https
+	LocalAssets  string        //本地静态资源目录 示例: /app/assets/   http://127.0.0.1:8888/demo/demo.html -> /app/assets/demo/demo.html
+	AssetsFSName string        //静态资源内置FS目录名 默认值: resources
+	Assets       emfs.IEmbedFS //静态资源内置FS目录对象
+	IP           string        //默认值: 127.0.0.1
+	PORT         int           //默认值: 80
+	SSL          *SSL          //设置后启动https
 }
 
-//证书配置，根据 Assets 或 LocalAssets 寻找证书文件位置
+// SSL 证书配置，根据 Assets 或 LocalAssets 寻找证书文件位置
 type SSL struct {
 	SSLCert string
 	SSLKey  string
@@ -40,21 +74,26 @@ func init() {
 	}
 }
 
+// NewAssetsHttpServer
+//
+//	创建静态资源http服务
 func NewAssetsHttpServer() *assetsHttpServer {
 	return &assetsHttpServer{
-		IP:   "127.0.0.1",
-		PORT: 80,
+		AssetsFSName: "resources",
+		IP:           "0.0.0.0",
+		PORT:         80,
 	}
 }
 
-func (m *assetsHttpServer) serveTLS(addr string, handler http.Handler) error {
+func (m *assetsHttpServer) serveTLS(addr string, handler http.Handler) {
 	server := &http.Server{Addr: addr, Handler: handler}
 	if addr == "" {
 		addr = ":https"
 	}
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
-		return err
+		println("serverTLS Listen failed error:", err)
+		return
 	}
 	defer ln.Close()
 	var config = &tls.Config{}
@@ -76,11 +115,11 @@ func (m *assetsHttpServer) serveTLS(addr string, handler http.Handler) error {
 					return tls.Certificate{}, err
 				}
 			} else if m.LocalAssets != "" {
-				certPEMBlock, err = os.ReadFile(m.LocalAssets + certFile)
+				certPEMBlock, err = ioutil.ReadFile(m.LocalAssets + certFile)
 				if err != nil {
 					return tls.Certificate{}, err
 				}
-				keyPEMBlock, err = os.ReadFile(m.LocalAssets + keyFile)
+				keyPEMBlock, err = ioutil.ReadFile(m.LocalAssets + keyFile)
 				if err != nil {
 					return tls.Certificate{}, err
 				}
@@ -91,20 +130,49 @@ func (m *assetsHttpServer) serveTLS(addr string, handler http.Handler) error {
 			}
 			return tls.X509KeyPair(certPEMBlock, keyPEMBlock)
 		}
-		var err error
 		config.Certificates = make([]tls.Certificate, 1)
 		config.Certificates[0], err = loadX509KeyPair(m.SSL.SSLCert, m.SSL.SSLKey)
 		if err != nil {
-			return err
+			println("serverTLS loadX509KeyPair failed error:", err)
+			return
 		}
 	}
 	tlsListener := tls.NewListener(ln, config)
-	return server.Serve(tlsListener)
+	go func() {
+		if err = server.Serve(tlsListener); err != nil {
+			println("tls server listen end", err.Error())
+		}
+	}()
+	//go m.graceShutdown(server)
 }
 
-func (m *assetsHttpServer) StartHttpServer() error {
+func (m *assetsHttpServer) serve(addr string, handler http.Handler) {
+	server := &http.Server{Addr: addr, Handler: handler}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			println("server listen end", err.Error())
+		}
+	}()
+	//go m.graceShutdown(server)
+}
+
+func (m *assetsHttpServer) graceShutdown(server *http.Server) {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
+	s := <-c
+	println("notify signal", s.String())
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		println("http server grace shutdown failed error:", err)
+	}
+	//os.Exit(1)
+}
+
+// StartHttpServer 启动内置Http Server
+func (m *assetsHttpServer) StartHttpServer() {
 	if m.LocalAssets != "" {
-		m.LocalAssets = strings.ReplaceAll(m.LocalAssets, "\\", "/")
+		m.LocalAssets = strings.Replace(m.LocalAssets, "\\", "/", -1) // ReplaceAll
 		if strings.LastIndex(m.LocalAssets, "/") != len(m.LocalAssets)-1 {
 			m.LocalAssets = m.LocalAssets + "/"
 		}
@@ -112,16 +180,19 @@ func (m *assetsHttpServer) StartHttpServer() error {
 	addr := fmt.Sprintf("%s:%d", m.IP, m.PORT)
 	mux := http.NewServeMux()
 	mux.Handle("/", m)
-	var err error
 	if m.SSL != nil {
-		err = m.serveTLS(addr, mux)
+		m.serveTLS(addr, mux)
 	} else {
-		err = http.ListenAndServe(addr, mux)
+		m.serve(addr, mux)
 	}
-	return err
 }
 
 func (m *assetsHttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if AssetsServerHeaderKeyValue != "" {
+		if AssetsServerHeaderKeyValue != r.Header.Get(AssetsServerHeaderKeyName) {
+			return
+		}
+	}
 	defer func() {
 		if err := recover(); err != nil {
 		}
